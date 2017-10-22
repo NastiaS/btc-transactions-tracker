6863795208a24e47ecf19fc2a4c96df2b71d4899
NastiaS <anastasiasergienko@gmail.com>
2017-10-22 21:50:50 +0200

    electron initial window config changed

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -8,7 +8,7 @@ const url = require('url');
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 1024, height: 960});
 
     const startUrl = process.env.ELECTRON_START_URL || url.format({
             pathname: path.join(__dirname, '/../build/index.html'),
@@ -16,7 +16,6 @@ function createWindow() {
             slashes: true
         });
     mainWindow.loadURL(startUrl);
-    mainWindow.webContents.openDevTools();
 
     mainWindow.on('closed', function () {
         mainWindow = null
